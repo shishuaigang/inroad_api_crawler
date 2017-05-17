c882ba088e286064ecc8de2ff487a49d3180935a
shishuaigang <shishuaigang@163.com>
2017-05-17 17:08:44 +0800

    修改json文件，修改读取参数方法

--- Test_scripts/api_param.py
+++ Test_scripts/api_param.py
@@ -19,51 +19,76 @@ class api_num:
             json_file = files[2]
         return json_file
 
-    def every_json_api_number(self):  # 每个json文件下有多少个API，存入list
+    def read_section(self):  # 返回每个section下value的值
         os.chdir(self.path)
-        return [len((json.loads(codecs.open(self.json_data[i], encoding='utf-8').read()))["apis"])
-                for i in range(len(self.json_data))]
+        re = []
+        temp = [(json.loads(codecs.open(self.json_data[i], encoding='utf-8').read()))["section"]
+                for i in range(len(self.json_data))]  # 结构[[{},{},{}],[{}]]
+        for i in range(len(self.json_data)):
+            result = []
+            for j in range(len(temp[i])):
+                result.append(temp[i][j]['value'])  # i=0时，result的第一个元素插入了temp[i]下面的所有value的值
+            re.append(result)
+        return re
+
+    def every_json_api_number(self):  # 每个json文件下有多少个API，存入list
+        # 返回格式[[15], [27, 6, 4, 3, 6, 5, 3, 8, 4, 3, 18, 6, 6], [63], [38]]
+        os.chdir(self.path)
+        l = self.read_section()
+        re = []
+        for i in range(len(self.json_data)):
+            result = []
+            for j in range(len(l[i])):
+                result.append(len(json.loads(codecs.open(self.json_data[i], encoding='utf-8').read())[l[i][j]]))
+            re.append(result)
+        return re
 
 
 class api_url(api_num):
     def api_url(self):
-        count = self.every_json_api_number()
+        num = self.every_json_api_number()
+        array_name = self.read_section()
         url = []
         for m in range(len(self.json_data)):
             f = codecs.open(self.json_data[m], encoding='utf-8')
             dict_json = json.loads(f.read())
             if "NoNeed" not in dict_json.keys() or dict_json["NoNeed"] == str(0):
-                for n in range(count[m]):
-                    if "NoNeed" not in dict_json["apis"][n] or dict_json["apis"][n]["NoNeed"] == str(0):
-                        url.append(dict_json["apis"][n]["url"])
+                for n in range(len(num[m])):  # num[m]是一个list
+                    for k in range(num[m][n]):
+                        if "NoNeed" not in dict_json[array_name[m][n]][k] or dict_json[array_name[m][n]][k]["NoNeed"] == str(0):
+                            url.append(dict_json[array_name[m][n]][k]["url"])
         return url
 
 
 class api_cn_name(api_num):
     def api_chinese_name(self):
-        count = self.every_json_api_number()
+        num = self.every_json_api_number()
+        array_name = self.read_section()
         api_chinese_name = []
         for m in range(len(self.json_data)):
             f = codecs.open(self.json_data[m], encoding='utf-8')
             dict_json = json.loads(f.read())
             if "NoNeed" not in dict_json.keys() or dict_json["NoNeed"] == str(0):
-                for n in range(count[m]):
-                    if "NoNeed" not in dict_json["apis"][n] or dict_json["apis"][n]["NoNeed"] == str(0):
-                        api_chinese_name.append(dict_json["apis"][n]["summary"])  # api的中文名字
+                for n in range(len(num[m])):
+                    for k in range(num[m][n]):
+                        if "NoNeed" not in dict_json[array_name[m][n]][k] or dict_json[array_name[m][n]][k]["NoNeed"] == str(0):
+                            api_chinese_name.append(dict_json[array_name[m][n]][k]["summary"])  # api的中文名字
         return api_chinese_name
 
 
 class api_cor_params(api_url):
     def api_details(self):
-        count = self.every_json_api_number()
+        num = self.every_json_api_number()
+        array_name = self.read_section()
         param_all_details = []
         for m in range(len(self.json_data)):
             f = codecs.open(self.json_data[m], encoding='utf-8')
             dict_json = json.loads(f.read())
             if "NoNeed" not in dict_json.keys() or dict_json["NoNeed"] == str(0):
-                for n in range(count[m]):
-                    if "NoNeed" not in dict_json["apis"][n] or dict_json["apis"][n]["NoNeed"] == str(0):
-                        param_all_details.append(dict_json["apis"][n]["params"])
+                for n in range(len(num[m])):
+                    for k in range(num[m][n]):
+                        if "NoNeed" not in dict_json[array_name[m][n]][k] or dict_json[array_name[m][n]][k]["NoNeed"] == str(0):
+                            param_all_details.append(dict_json[array_name[m][n]][k]["params"])
         return param_all_details
 
     def api_correct_params(self):
